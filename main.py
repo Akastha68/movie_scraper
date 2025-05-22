6d5a65da2ec35ae2b1f731875ec9f6ac6dd618da
Akash Patel <superficialak60@yahoo.com>
2025-05-22 06:48:49 +0530

    update

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from config import filmyfly
 from db import setup_tables
 from scraper import Scrape
+from urllib.parse import urljoin
+import time
 
 def main():
     setup_tables()
-    scraper = Scrape(filmyfly)
-    scraper.scrape()
-    print("[INFO] Scraping and database storage completed successfully.")
-
+    i=11
+    while (i<=82):
+        url = filmyfly+str(i)
+        print(f"[INFO] Scraping started on {url}")
+        scraper = Scrape(url)
+        scraper.scrape()
+        print("[INFO] Scraping and database storage completed successfully.")
+        i+=1
 if __name__ == "__main__":
     main()
